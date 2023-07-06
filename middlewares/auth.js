6c2bff8e63e3cef1d732b217a6892310ac3df9cc
gulnaz0206 <113769277+gulnaz0206@users.noreply.github.com>
2023-07-06 21:22:30 +0500

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    next(new Unauthorized('Необходима авторизация'));
+    return next(new Unauthorized('Необходима авторизация'));
   }
   req.user = payload;
   next();
